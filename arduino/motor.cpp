317ccbd042343d51d265f56e1e52fd043580e6c9
David ADLER <david.adler@outlook.com>
2018-12-04 13:29:13 +0100

    version A.002
    1) Ajouts du mode continue
    2) possiblité d'utilisé la connection serie de l'arduino au lieux de soltwareSerial (supprimmé le #define SOFTSERIAL)
    
    David ADLER <david.adler@outlook.com>

--- arduino/motor.cpp
+++ arduino/motor.cpp
@@ -13,34 +13,57 @@ motor::motor(int p1, int p2, int p3, int p4){
 }
 
 void motor::go_pas(long count, long pas_t){
+  chmode(0);
   _go_pas = count;
   _pas_t  = pas_t < abs(count) ? abs(count) : pas_t;
   _pos_p  = 0;
   _pos_t  = 0;
 }
 
+void motor::go_continue(int x){
+  chmode(x);
+  _go_pas=x>0?x:-x;
+}
+void motor::chmode(int x){
+  if(x == 0)    _mode = 0;
+  else if (x>0) _mode = 1;
+  else if (x<0) _mode = 2;
+}
+
 bool motor::go(){
-  int mode;
-  if (_pos_t == _pas_t) return false;
-  long p = (_pos_t * _go_pas)/_pas_t ;
-  _pos_t++;
-  if(_pos_p == p){
-    if(_go_pas > 0){
-      if(_go_pas > p){
-        mode  = 1;
-        _pos_p ++;
-        _pas_from_o ++;        
-      }
-    }
-    else{
-     if(_go_pas < p){
-       mode  = -1;
-        _pos_p --;
-        _pas_from_o --;
-      
-     }
-    }
-    _pos += mode;
+  int change = 0;
+  switch(_mode){
+    case 1:
+    case 2:
+        if(_go_pas<=_pos_p){
+          change = _mode == 1? 1:-1;
+          _pos_p=0;
+        }
+        else 
+          _pos_p+=1;
+        break;
+    default:
+        if (_pos_t == _pas_t) return false;
+        long p = (_pos_t * _go_pas)/_pas_t ;
+        _pos_t++;
+        if(_pos_p == p){
+          if(_go_pas > 0){
+            if(_go_pas > p){
+              change  = 1;
+              _pos_p ++;
+            }
+          }
+          else{
+           if(_go_pas < p){
+             change  = -1;
+              _pos_p --;
+           }
+          }
+        }
+  }
+  if(change !=0){
+    _pos += change;
+    _pas_from_o += change;
     if(_pos >= MOTOR_PAS) _pos = 0;
     if(_pos <  0)         _pos = MOTOR_PAS - 1;  
     for(int i=0; i<MOTOR_PIN; i++){
